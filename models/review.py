f1ab476ef170c8131e6100b3fe6985cf2dd491f0
MundiaNderi <sarahngima77@gmail.com>
2023-04-19 23:59:19 +0300

    Tasks

--- models/review.py
+++ models/review.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    """ Review classto store review information
+    place_id = place id
+    user_id = user id
+    text = review description
+    """
+
+    __tablename__ = "reviews"
+    text = Column(String(1024),
+                  nullable=False)
+    place_id = Column(String(60),
+                      ForeignKey("places.id", ondelete="CASCADE"),
+                      nullable=False)
+    user_id = Column(String(60),
+                     ForeignKey("users.id", ondelete="CASCADE"),
+                     nullable=False)
