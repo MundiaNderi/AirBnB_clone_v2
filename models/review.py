232267675cede44568d5ccc77ba46ffba01c5c35
MundiaNderi <sarahngima77@gmail.com>
2023-04-22 22:51:26 +0300

    Module Not Found Fixes

--- models/review.py
+++ models/review.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy import Column, String
+import models
 
 
-class Review(BaseModel):
-    """ Review classto store review information
-    place_id = place id
-    user_id = user id
-    text = review description
+class Review(BaseModel, Base):
+    """ Review classto store review information.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store Reviews.
+        text (sqlalchemy String): The review description.
+        place_id (sqlalchemy String): The review's place id.
+        user_id (sqlalchemy String): The review's user id.
     """
-
     __tablename__ = "reviews"
-    text = Column(String(1024),
-                  nullable=False)
-    place_id = Column(String(60),
-                      ForeignKey("places.id", ondelete="CASCADE"),
-                      nullable=False)
-    user_id = Column(String(60),
-                     ForeignKey("users.id", ondelete="CASCADE"),
-                     nullable=False)
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
