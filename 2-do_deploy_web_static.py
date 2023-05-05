94c032c017486ae1b33446ad98f9815dba7c13cb
MundiaNderi <sarahngima77@gmail.com>
2023-05-06 00:31:53 +0300

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-# Module with fabric script that distributes an archive to server.
+# module with Fabric script that distributes an archive to server
 
 from fabric.api import *
 import os.path
 
 env.user = 'ubuntu'
-env.hosts = ['107.22.143.207', '100.25.200.19']
+env.hosts = ['35.231.100.106', '35.237.151.115']
 
 
 def do_deploy(archive_path):
-    """Fabric script that distributes an archive to server"""
+    """Fabric script that distrubutes an archive to server"""
     if os.path.exists(archive_path):
         new_path = archive_path.replace('versions/', '')
-        file_name = new_path[:4]
-        arc_folder = "/data/web_static/release/{}".format(new_path)
+        file_name = new_path[:-4]
+        arc_folder = "/data/web_static/releases/{}".format(new_path)
         put(archive_path, '/tmp/')
         run('mkdir -p /data/web_static/releases/{}'.format(file_name))
         run('tar -xzf /tmp/{} -C {}'.format(new_path, arc_folder[:-4]))
         run('rm /tmp/{}'.format(new_path))
-        run('mv {}web_static/* {}/'.format(arc_folder[:-4], arc_folder[:-4]))
+        run('mv {}/web_static/* {}/'.format(arc_folder[:-4], arc_folder[:-4]))
         run('rm -rf {}/web_static'.format(arc_folder[:-4]))
         run('rm -rf /data/web_static/current')
         run('ln -s {} /data/web_static/current'.format(arc_folder[:-4]))
