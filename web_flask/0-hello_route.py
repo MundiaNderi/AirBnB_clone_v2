8ef6270142331cbf498bd91c17e7b52a609d3669
MundiaNderi <sarahngima77@gmail.com>
2023-05-20 09:56:56 +0300

    Hello Flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from flask import Flask
+app = Flask(__name__)
+app.url_mpa.strict_slashes = False
+ip = '0.0.0.0'
+port = 5000
+
+
+@app.route('/')
+def hello_hbnb():
+    # says Hello HBNB when curl'd
+    return ("Hello HBNB!")
+
+
+if__name__ == "__main__":
+app.run(host=ip, port=port)
