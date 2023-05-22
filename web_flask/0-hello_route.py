c1705ab239839e5b76ca1637e6c572a54905a6ab
MundiaNderi <sarahngima77@gmail.com>
2023-05-22 08:55:26 +0300

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 from flask import Flask
+
+# Create a Flask web application
 app = Flask(__name__)
+
+# Disable strict slashes at the end
 app.url_map.strict_slashes = False
+
+# Define the IP address and port for the server
 ip = '0.0.0.0'
 port = 5000
 
 
+# Define a route for the root URL
 @app.route('/')
 def hello_hbnb():
-    # says Hello HBNB when curl'd
+    # returns Hello HBNB when curl'd
     return ("Hello HBNB!")
 
 
+# Start the Flask application if this script is executed directly
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
