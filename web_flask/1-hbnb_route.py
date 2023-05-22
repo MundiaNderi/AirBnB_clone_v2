b51c097e5094a5a21283276c570caa17cf63fe37
MundiaNderi <sarahngima77@gmail.com>
2023-05-22 09:10:38 +0300

    Task 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
 # starts a Flask web app
 from flask import Flask
+
+# Create a Flask Web Application
 app = Flask(__name__)
-app.url_map.strict_slashes = Falseip = '0.0.0.0'
+
+# Disable strict slashes at the end
+app.url_map.strict_slashes = False
+
+# Define the IP address and port of the server
+ip = '0.0.0.0'
 port = 5000
 
 
 @app.route('/')
 def hello_hbnb():
-    # says Hello HBNB when curl'd
+    """Route handler function for the root URL.
+    Returns the string "Hello HBNB!" when the root URL is accessed.
+    """
     return ("Hello HBNB!")
 
 
 @app.route('/hbnb')
 def hbnb():
-    # displays HBNB
+    """
+    Route handler function for the root URL.
+    Returns the string "Hello HBNB!" when the root URL is accessed.
+    """
     return ("HBNB")
 
 
+# # Start the Flask application if this script is executed directly
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
