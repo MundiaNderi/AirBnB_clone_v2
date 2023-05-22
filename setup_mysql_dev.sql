b41081b2ff6ab4f894f210ca53324b6fb4df7a2e
MundiaNderi <sarahngima77@gmail.com>
2023-05-22 22:20:30 +0300

    Editing backticks

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,14 @@
 -- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
-GRANT ALL PRIVILEGES ON hbnb_dev.* TO 'hbnb_dev'@'localhost';
+-- Grant all privileges on the hbnb_test_db database to the hbnb_test user
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
--- Grant Select privilege on the perfomance_schema database to the hbnb_dev user
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Grant Select privilege on the performance_schema database to the hbnb_test user
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 -- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
