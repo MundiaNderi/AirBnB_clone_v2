e1cfca104ec4622995dc19211572218e9491ef3f
MundiaNderi <sarahngima77@gmail.com>
2023-05-28 12:43:02 +0300

    Fixing sql comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create the user if it doesn't exist
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant all privileges on the hbnb_test_db database to the hbnb_dev user
+-- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
 GRANT ALL PRIVILEGES ON hbnb_dev.* TO 'hbnb_dev'@'localhost';
 
 -- Grant select privilege on the perfomance_schema database to the hbnb_dev user
