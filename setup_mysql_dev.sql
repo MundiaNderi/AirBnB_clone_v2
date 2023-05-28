2604d5af112db70012c1d11c0b02ace06a701ec3
MundiaNderi <sarahngima77@gmail.com>
2023-05-28 12:30:19 +0300

    Fixing sql scripts

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,14 +1,14 @@
 -- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create the user if it doesn't exist
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant all privileges on the hbnb_test_db database to the hbnb_test user
-GRANT ALL PRIVILEGES ON hbnb_dev.* TO 'hbnb_test'@'localhost';
+-- Grant all privileges on the hbnb_test_db database to the hbnb_dev user
+GRANT ALL PRIVILEGES ON hbnb_dev.* TO 'hbnb_dev'@'localhost';
 
--- Grant Select privilege on the perfomance_schema database to the hbnb_test user
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- Grant select privilege on the perfomance_schema database to the hbnb_dev user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 -- Flush privileges to apply the changes
 FLUSH PRIVILEGES;
